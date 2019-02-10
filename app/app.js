4ac4d7bc56d0a77905aa9aaa753ddb07285a3fe4
atteeqahmad <atteeqahmad17@gmail.com>
2019-02-10 12:19:02 -0700

    .

--- app/app.js
+++ app/app.js
@@ -1,3 +1,44 @@
-function slap {
-    slap
+let total = 0;
+
+let target = {
+    name: Tom,
+    Health: 100,
+    Hit: 0,
+
+}
+
+function slap() {
+    health: --1,
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SlapSlap = {
+    Slap: {
+        level: 100--,
+        img: 'assets/slap.jpg',
+    }
+
+
 }
