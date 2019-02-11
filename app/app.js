2158ff49b4b7f619db84d1487a1fc6ac47eba0a3
atteeqahmad <atteeqahmad17@gmail.com>
2019-02-11 00:05:11 -0700

    ..

--- app/app.js
+++ app/app.js
@@ -1,44 +1,60 @@
-let total = 0;
+
+let items = {
+    sword: {
+        name: 'sword',
+        modifier: 35,
+        description: 'cut'
+    },
+    hammer: {
+        name: 'hammer',
+        modifier: 25,
+        description: 'bang'
+    },
+    cheese: {
+        name: 'cheese',
+        modifier: 50,
+        description: 'yumenergy'
+    },
+}
+
+function giveItems(item) {
+    target.items.push(items[item].modifier)
+}
+function addMods() {
+    let
+
+
+}
+
 
 let target = {
-    name: Tom,
-    Health: 100,
-    Hit: 0,
+    name: "Tom",
+    health: 100,
+    hit: 0,
+    items: []
 
 }
 
 function slap() {
-    health: --1,
+    target.health -= 1;
+    target.hit++;
+    update();
+}
 
+function punch() {
+    target.health -= 5;
+    target.hit++;
+    update();
+}
+
+function kick() {
+    target.health -= 15;
+    target.hit++;
+    update();
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SlapSlap = {
-    Slap: {
-        level: 100--,
-        img: 'assets/slap.jpg',
-    }
-
-
+function update() {
+    document.getElementById('health').innerText = target.health.toString();
+    document.getElementById('hit').innerText = target.hit.toString();
 }
