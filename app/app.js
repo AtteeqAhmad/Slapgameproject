3dab13dd47c4d0d2f6ce406a53e0ea3656c9a56f
atteeqahmad <atteeqahmad17@gmail.com>
2019-02-11 13:53:36 -0700

    f.....

--- app/app.js
+++ app/app.js
@@ -2,53 +2,57 @@
 let items = {
     sword: {
         name: 'sword',
-        modifier: 35,
+        modifier: 3,
         description: 'cut'
     },
     hammer: {
         name: 'hammer',
-        modifier: 25,
+        modifier: 5,
         description: 'bang'
     },
     cheese: {
         name: 'cheese',
-        modifier: 50,
+        modifier: 35,
         description: 'yumenergy'
     },
 }
 
-function giveItems(item) {
-    target.items.push(items[item].modifier)
-}
-
+function giveItem(item) {
+    //target.items.push(items[item].modifier)
+    target.modifier += items[item].modifier;
 }
 
 let target = {
     name: "Tom",
     health: 100,
     hit: 0,
-    items: []
-
+    items: [],
+    modifier: 0
 }
 
 function slap() {
-    target.health -= 1;
+    target.health -= 1 + target.modifier;
     target.hit++;
     update();
 }
 
 function punch() {
-    target.health -= 5;
+    target.health -= 3 + target.modifier;
     target.hit++;
     update();
 }
 
 function kick() {
-    target.health -= 15;
+    target.health -= 5 + target.modifier;
     target.hit++;
     update();
 }
 
+function power() {
+    target.health += 10 + target.modifier;
+    target.hit = 0;
+    update();
+}
 
 function update() {
     document.getElementById('health').innerText = target.health.toString();
